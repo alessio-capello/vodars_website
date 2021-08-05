c3f39b2577e0472270dd773849cfc22c2bb9496f
alessio-capello <alessio.capello.93@gmail.com>
2021-08-05 21:56:37 +0200

    dropdown animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import { NavLink } from "react-router-dom";
 import $ from "jquery";
 import Logo512 from "../../images/Logo512.png";
 
 const Navbar = () => {
+  const [shownID, setShownId] = useState("");
   function animation() {
     var tabsNewAnim = $("#navbarSupportedContent");
     var activeItemNewAnim = tabsNewAnim.find(".active");
@@ -55,6 +56,24 @@ const Navbar = () => {
     });
   }
 
+  function showDropdown(id) {
+    if (shownID !== "" && (id === "" || id === shownID)) {
+      //reset last menu
+      $("#" + shownID).animate({ height: "0px" }, 250, () => {
+        $("#" + shownID).hide();
+      });
+
+      setShownId("");
+    }
+
+    if (id !== "" && id !== shownID) {
+      //display the new menu
+      $("#" + id).show();
+      $("#" + id).animate({ height: "70px" }, 300);
+
+      setShownId(id);
+    }
+  }
   useEffect(() => {
     animation();
     $(window).on("resize", function () {
@@ -65,71 +84,148 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-mainbg">
-      <NavLink to="/" exact className="navbar-brand navbar-logo" id="navLogo">
-        <img
-          src={Logo512}
-          alt="logo.png"
-          width="45"
-          height="45"
-          style={{ borderRadius: "50%", margin: "0" }}
-        />
-      </NavLink>
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-mainbg">
+        <NavLink
+          to="/"
+          exact
+          className="navbar-brand navbar-logo"
+          id="navLogo"
+          onClick={() => {
+            showDropdown("");
+          }}
+        >
+          <img
+            src={Logo512}
+            alt="logo.png"
+            width="46"
+            height="46"
+            style={{ borderRadius: "22px", margin: "0" }}
+          />
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          onClick={function () {
+            setTimeout(function () {
+              animation();
+            });
+          }}
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars text-white"></i>
+        </button>
+        <div className="midsection">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <div className="hori-selector">
+                <div className="left"></div>
+                <div className="right"></div>
+              </div>
 
-      <button
-        className="navbar-toggler"
-        onClick={function () {
-          setTimeout(function () {
-            animation();
-          });
-        }}
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fas fa-bars text-white"></i>
-      </button>
+              <li className="nav-item active" id="homeLink">
+                <NavLink
+                  className="nav-link"
+                  to="/"
+                  exact
+                  onClick={() => {
+                    showDropdown("");
+                  }}
+                >
+                  <i className="fas fa-tachometer-alt"></i>Home
+                </NavLink>
+              </li>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <div className="hori-selector">
-            <div className="left"></div>
-            <div className="right"></div>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/about"
+                  exact
+                  onClick={() => {
+                    showDropdown("");
+                  }}
+                >
+                  <i className="far fa-address-book"></i>About
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="#"
+                  exact
+                  onClick={() => {
+                    showDropdown("dropdown_1");
+                  }}
+                >
+                  TEST DROPDOWN
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/service"
+                  exact
+                  onClick={() => {
+                    showDropdown("");
+                  }}
+                >
+                  <i className="far fa-clone"></i>Services
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/testimonial"
+                  exact
+                  onClick={() => {
+                    showDropdown("");
+                  }}
+                >
+                  <i className="far fa-chart-bar"></i>Testimonial
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/contact"
+                  exact
+                  onClick={() => {
+                    showDropdown("");
+                  }}
+                >
+                  <i className="far fa-copy"></i>Contact Us
+                </NavLink>
+              </li>
+            </ul>
           </div>
-
-          <li className="nav-item active" id="homeLink">
-            <NavLink className="nav-link" to="/" exact>
-              <i className="fas fa-tachometer-alt"></i>Home
-            </NavLink>
-          </li>
-
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/about" exact>
-              <i className="far fa-address-book"></i>About
-            </NavLink>
-          </li>
-
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/service" exact>
-              <i className="far fa-clone"></i>Services
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/testimonial" exact>
-              <i className="far fa-chart-bar"></i>Testimonial
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/contact" exact>
-              <i className="far fa-copy"></i>Contact Us
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <div className="hidden-menu" id="dropdown_1">
+            <ul className="ul-hidden">
+              <li className="li-hidden">
+                <NavLink className="navlink-hidden" to="/a" exact>
+                  <i className="far fa-copy"></i>Aaaaa
+                </NavLink>
+              </li>
+              <li className="li-hidden">
+                <NavLink className="navlink-hidden" to="/a" exact>
+                  <i className="far fa-copy"></i>Bbbbb
+                </NavLink>
+              </li>
+              <li className="li-hidden">
+                <NavLink className="navlink-hidden" to="/a" exact>
+                  <i className="far fa-copy"></i>Ccccc
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
   );
 };
 export default Navbar;
